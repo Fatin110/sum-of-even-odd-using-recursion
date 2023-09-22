009b99d5ac9bb2c9270c662dc828fbc46555f68d
Fatin110 <ishmam110@gmail.com>
2023-09-22 19:18:18 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	function evenOdd ($start, $end) {
+	  static $evenSum = 0;
+	  static $oddSum = 0;
+	  
+    if ($start > $end){
+      echo "Even number sum is $evenSum\n";
+      echo "Odd number sum is $oddSum\n";
+      return;
+    }
+    
+	  if ($start % 2 == 0){
+	    $evenSum = $evenSum + $start;
+      evenOdd($start+1, $end);
+	  }
+	  
+	  else {
+	    $oddSum = $oddSum + $start;
+	    evenOdd($start+1, $end);
+	  }
+	}
+	
+	evenOdd(1,20);
+?>
